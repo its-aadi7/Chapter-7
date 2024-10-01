7e108811b4de18fd83e7591b077c3bb65b7f6313
its-aadi7 <dedhiaaadi7@gamil.com>
2024-10-01 17:29:34 +0530

    Today

---
+++ 07_break_and_continuous.py
@@ -0,0 +1,11 @@
+## For Loop with break
+for i in range(100):
+    if (i == 34):
+        break # Exit the loop right now
+    print(i)
+
+## For Loop wiht continue
+for i in range(100):
+    if (i == 34):
+        continue # skip this iteration  
+    print(i)
