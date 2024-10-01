eada2f92dbab22edfae90196cd972fd6cbb7fcc6
its-aadi7 <dedhiaaadi7@gamil.com>
2024-10-01 16:59:26 +0530

    todays

---
+++ 05_for_loop_iterate.py
@@ -0,0 +1,16 @@
+## For Loop with Lists
+l = [1, 4, 6, 234, 6, 764]
+
+for i in l:
+    print(i)
+
+## For Loop with Tuples
+t = (6, 231, 75, 122)
+
+for i in t:
+    print(i)
+
+## For loop with Strings
+a = "Aadi"
+for i in a:
+    print(i)
